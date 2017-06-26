3c9a30d7b27c76df398fff7240fe129d3139615f
Gil Serra <gilserrap@gmail.com>
2017-06-26 11:49:55 +0200

    Initial Commit

---
+++ Matcha/Matcha.h
@@ -0,0 +1,19 @@
+//
+//  Matcha.h
+//  Matcha
+//
+//  Created by Gil Serra on 26/06/2017.
+//  Copyright © 2017 Schibsted Spain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Matcha.
+FOUNDATION_EXPORT double MatchaVersionNumber;
+
+//! Project version string for Matcha.
+FOUNDATION_EXPORT const unsigned char MatchaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matcha/PublicHeader.h>
+
+
