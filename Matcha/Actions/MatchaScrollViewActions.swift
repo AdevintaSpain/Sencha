966f84320e7f516e97dbe2aaa2780c0cb51ff73b
Gil Serra <gilserrap@gmail.com>
2017-07-17 11:38:10 +0200

    Added capability of scrolling a certain distance

--- Matcha/Actions/MatchaScrollViewActions.swift
+++ Matcha/Actions/MatchaScrollViewActions.swift
@@ -6,6 +6,7 @@ public protocol MatchaScrollViewActions: EarlGreyHumanizer {
     
     func tap(_ matcher: Matcher, inScrollableElementWith scrollMatcher: Matcher, file: StaticString, line: UInt)
     @discardableResult func scrollTo(_ matcher: Matcher, inElementWith scrollMatcher: Matcher, file: StaticString, line: UInt) -> GREYElementInteraction
+    @discardableResult func scrollTo(_ matcher: Matcher, withSwipesOf distance: CGFloat, inElementWith scrollMatcher: Matcher, file: StaticString, line: UInt) -> GREYElementInteraction
 }
 
 public extension MatchaScrollViewActions {
@@ -25,15 +26,26 @@ public extension MatchaScrollViewActions {
     @discardableResult func scrollTo(_ matcher: Matcher, inElementWith scrollMatcher: Matcher, file: StaticString = #file, line: UInt = #line) -> GREYElementInteraction {
         
         let halfScreenSize = UIScreen.main.bounds.height/2
+        return scrollTo(
+            matcher,
+            withSwipesOf: halfScreenSize,
+            inElementWith: scrollMatcher,
+            file: file,
+            line: line
+        )
+    }
+    
+    @discardableResult func scrollTo(_ matcher: Matcher, withSwipesOf distance: CGFloat, inElementWith scrollMatcher: Matcher, file: StaticString = #file, line: UInt = #line) -> GREYElementInteraction {
+        
         return select(
             matcher: matcher,
             file: file,
             line: line
         ).usingSearch(
-            grey_scrollInDirection(GREYDirection.down, halfScreenSize),
+            grey_scrollInDirection(GREYDirection.down, CGFloat(distance)),
             onElementWith: grey_allOf([
-                    scrollMatcher.greyMatcher(),
-                    Matcher.visible.greyMatcher()
+                scrollMatcher.greyMatcher(),
+                Matcher.visible.greyMatcher()
                 ]
             )
         )
