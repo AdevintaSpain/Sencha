d7d9f5948bcef2db223a6b2aaad1f297da990a05
Gil Serra <gilserrap@gmail.com>
2017-09-08 11:50:27 +0200

    Updated example with the usage of textFields

--- ExampleProject/DetailViewController.swift
+++ ExampleProject/DetailViewController.swift
@@ -3,9 +3,17 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    struct AccessibilityID {
+
+        static let textField = "DetailViewController.textField"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Detail"
+        textField.accessibilityIdentifier = AccessibilityID.textField
     }
 }
--- ExampleProjectTests/DetailViewShould.swift
+++ ExampleProjectTests/DetailViewShould.swift
@@ -18,4 +18,36 @@ class ViewDetailShould: XCTestCase {
         assertEnabled(.text("This is an enabled button"))
         assertDisabled(.text("This is a disabled button"))
     }
+
+    func test_show_an_interactable_text_field() {
+
+        assertVisible(.text("Some placeholder text"))
+
+        type(
+            text: "I am typing stuff!!",
+            inElementWith: .accessibilityID(DetailViewController.AccessibilityID.textField)
+        )
+
+        /*  
+         Some UI elements have unintuitive view hieracies, i.e.: The UITextField contains a UITextView for typing and
+         a UILabel for displaying text, in this case, if we search the view hieracy with only the text, it finds
+         multiple elements.
+         
+         In this case we can use the .allOf matcher in combination with .firstElement matcher, because we know there
+         are multiple elements, but we don't care as long as the text exists.
+        */
+        assertVisible(
+            .allOf([.text("I am typing stuff!!"), .firstElement])
+        )
+
+        /*
+         Also, we can specify the exact element where we want to serach for the text, using the primitive select
+         and assert functions:
+        */
+        select(
+            matcher: .accessibilityID(DetailViewController.AccessibilityID.textField)
+        ).assert(
+            .text("I am typing stuff!!")
+        )
+    }
 }
