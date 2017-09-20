e5446111fb335faf865e457c5eb11e9ee81fccc3
Gil Serra <gilserrap@gmail.com>
2017-09-20 12:50:25 +0200

    Added tap back button action

--- Matcha/Actions/MatchaTapActions.swift
+++ Matcha/Actions/MatchaTapActions.swift
@@ -5,6 +5,7 @@ import EarlGrey
 public protocol MatchaTapActions: EarlGreyHumanizer {
     
     func tap(_ matcher: Matcher, file: StaticString, line: UInt)
+    func tapBackButton(file: StaticString, line: UInt)
 }
 
 public extension MatchaTapActions {
@@ -13,6 +14,17 @@ public extension MatchaTapActions {
         
         select(
             matcher,
+            file: file,
+            line: line
+        ).perform(
+            grey_tap()
+        )
+    }
+
+    public func tapBackButton(file: StaticString = #file, line: UInt = #line) {
+
+        select(
+            matcher: .class(NSClassFromString("_UIBackButtonContainerView")!),
             file: file,
             line: line
         ).perform(
