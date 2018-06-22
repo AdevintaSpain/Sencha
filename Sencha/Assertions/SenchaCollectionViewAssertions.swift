eff61865770d2f35a76378ae91eb75330977a45a
Gil Serra <gilserrap@gmail.com>
2018-06-22 13:01:25 +0200

    Improve naming

--- Sencha/Assertions/SenchaCollectionViewAssertions.swift
+++ Sencha/Assertions/SenchaCollectionViewAssertions.swift
@@ -4,15 +4,15 @@ import EarlGrey
 
 public protocol SenchaCollectionViewAssertions: EarlGreyHumanizer {
 
-    func assert(collectionViewWith matcher: Matcher, hasRowCount rowCount: Int, file: StaticString, line: UInt)
-    func assert(collectionViewWith matcher: Matcher, hasRowCount rowCount: Int, inSection section: Int, file: StaticString, line: UInt)
+    func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, file: StaticString, line: UInt)
+    func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, inSection section: Int, file: StaticString, line: UInt)
     func assert(collectionViewWith matcher: Matcher, hasSectionCount sectionCount: Int, file: StaticString, line: UInt)
 
 }
 
 public extension SenchaCollectionViewAssertions {
 
-    func assert(collectionViewWith matcher: Matcher, hasRowCount rowCount: Int, file: StaticString = #file, line: UInt = #line) {
+    func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, file: StaticString = #file, line: UInt = #line) {
 
         assert(
             collectionViewWith: matcher,
@@ -23,19 +23,19 @@ public extension SenchaCollectionViewAssertions {
 
         assert(
             collectionViewWith: matcher,
-            hasRowCount: rowCount,
+            hasCellCount: cellCount,
             inSection: 0,
             file: file,
             line: line
         )
     }
 
-    func assert(collectionViewWith matcher: Matcher, hasRowCount rowCount: Int, inSection section: Int, file: StaticString = #file, line: UInt = #line) {
+    func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, inSection section: Int, file: StaticString = #file, line: UInt = #line) {
 
         let cellCountAssert = GREYAssertionBlock(name: "cell count") { (element, error) -> Bool in
             if let collectionView = element as? UICollectionView {
                 let numberOfCells = collectionView.numberOfItems(inSection: section)
-                return numberOfCells == rowCount
+                return numberOfCells == cellCount
             }
             return false
         }
