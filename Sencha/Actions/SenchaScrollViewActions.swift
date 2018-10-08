e798d148bd12711b3f8b274d8af5bd0e665e69cf
Gil Serra <gilserrap@gmail.com>
2018-10-08 14:25:08 +0200

    Added content edges scrolling

--- Sencha/Actions/SenchaScrollViewActions.swift
+++ Sencha/Actions/SenchaScrollViewActions.swift
@@ -6,6 +6,10 @@ public protocol SenchaScrollViewActions: EarlGreyHumanizer {
     func tap(_ matcher: Matcher, inScrollableElementWith scrollMatcher: Matcher, file: StaticString, line: UInt)
     @discardableResult func scrollTo(_ matcher: Matcher, inElementWith scrollMatcher: Matcher, file: StaticString, line: UInt) -> GREYInteraction
     @discardableResult func scrollTo(_ matcher: Matcher, withSwipesOf distance: CGFloat, inElementWith scrollMatcher: Matcher, file: StaticString, line: UInt) -> GREYInteraction
+    func scrollToBottom(in matcher: Matcher, file: StaticString, line: UInt)
+    func scrollToTop(in matcher: Matcher, file: StaticString, line: UInt)
+    func scrollToLeft(in matcher: Matcher, file: StaticString, line: UInt)
+    func scrollToRight(in matcher: Matcher, file: StaticString, line: UInt)
 }
 
 public extension SenchaScrollViewActions {
@@ -22,7 +26,27 @@ public extension SenchaScrollViewActions {
             grey_tap()
         )
     }
-    
+
+    func scrollToBottom(in matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        scroll(to: .bottom, in: matcher)
+    }
+
+    func scrollToTop(in matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        scroll(to: .top, in: matcher)
+    }
+
+    func scrollToLeft(in matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        scroll(to: .left, in: matcher)
+    }
+
+    func scrollToRight(in matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        scroll(to: .right, in: matcher)
+    }
+
+    private func scroll(to edge: GREYContentEdge, in matcher: Matcher) {
+        EarlGrey.selectElement(with: matcher.greyMatcher()).perform(grey_scrollToContentEdge(edge))
+    }
+
     @discardableResult func scrollTo(_ matcher: Matcher, inElementWith scrollMatcher: Matcher, file: StaticString = #file, line: UInt = #line) -> GREYInteraction {
         
         let halfScreenSize = UIScreen.main.bounds.height/2
