0263cc9f09df37548b9e13ac2ccd26f073f5c9e9
Gil Serra <gilserrap@gmail.com>
2019-01-15 12:30:02 +0100

    Improved test syntax

--- ExampleProjectTests/MainViewShould.swift
+++ ExampleProjectTests/MainViewShould.swift
@@ -5,7 +5,9 @@ import Sencha
 @testable import ExampleProject
 
 class MainViewShould: XCTestCase {
-    
+
+    let TABLE_VIEW_MATCHER = Matcher.accessibilityID(MainViewController.AccessibilityID.tableView)
+
     let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: MainViewController.self))
     var viewController: MainViewController!
 
@@ -19,27 +21,27 @@ class MainViewShould: XCTestCase {
 
     func test_be_able_to_assert_TableView_is_empty() {
         viewController.clearTableView()
-        assertTableViewIsEmpty(with: .accessibilityID(MainViewController.AccessibilityID.tableView))
+        assertTableViewIsEmpty(with: TABLE_VIEW_MATCHER)
     }
 
     func test_be_able_to_assert_TableView_is_not_empty() {
 
-        assertTableViewIsNotEmpty(with: .accessibilityID(MainViewController.AccessibilityID.tableView))
+        assertTableViewIsNotEmpty(with: TABLE_VIEW_MATCHER)
     }
 
     func test_be_able_to_assert_the_number_of_rows() {
         
-        assert(tableViewWith: .accessibilityID(MainViewController.AccessibilityID.tableView), hasRowCount: 100)
+        assert(tableViewWith: TABLE_VIEW_MATCHER, hasRowCount: 100)
     }
     
     func test_be_able_to_assert_the_number_of_sections() {
         
-        assert(tableViewWith: .accessibilityID(MainViewController.AccessibilityID.tableView), hasSectionCount: 1)
+        assert(tableViewWith: TABLE_VIEW_MATCHER, hasSectionCount: 1)
     }
     
     func test_be_able_to_scroll_to_an_offscreen_cell_and_verify_the_content() {
     
-        assertVisible(.text("20"), inScrollableElementWith: .accessibilityID(MainViewController.AccessibilityID.tableView))
+        assertVisible(.text("20"), inScrollableElementWith: TABLE_VIEW_MATCHER)
     }
     
     func test_be_able_to_tap_elements_and_verify_the_result() {
@@ -49,8 +51,8 @@ class MainViewShould: XCTestCase {
     }
     
     func test_be_able_to_navigate_to_a_detail_view() {
-        
-        tap(.text("12"), inScrollableElementWith: .accessibilityID(MainViewController.AccessibilityID.tableView))
+
+        tap(.text("12"), inScrollableElementWith: TABLE_VIEW_MATCHER)
         assertVisible(.text("Detail"))
     }
 
@@ -58,7 +60,7 @@ class MainViewShould: XCTestCase {
 
         assertVisible(.text("Sencha Example"))
 
-        tap(.text("12"), inScrollableElementWith: .accessibilityID(MainViewController.AccessibilityID.tableView))
+        tap(.text("12"), inScrollableElementWith: TABLE_VIEW_MATCHER)
         assertVisible(.text("Detail"))
         tapBackButton()
 
