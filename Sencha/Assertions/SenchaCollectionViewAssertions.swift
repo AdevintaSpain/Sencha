ecb424e6446d27a9281d4f6b3c19c8583f58ebbf
Gil Serra <gilserrap@gmail.com>
2019-01-15 12:22:55 +0100

    Added empty /not empty TableView/CollectionView assertions

--- Sencha/Assertions/SenchaCollectionViewAssertions.swift
+++ Sencha/Assertions/SenchaCollectionViewAssertions.swift
@@ -4,6 +4,8 @@ import EarlGrey
 
 public protocol SenchaCollectionViewAssertions: EarlGreyHumanizer {
 
+    func assertCollectionViewIsEmpty(with matcher: Matcher, file: StaticString, line: UInt)
+    func assertCollectionViewIsNotEmpty(with matcher: Matcher, file: StaticString, line: UInt)
     func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, file: StaticString, line: UInt)
     func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, inSection section: Int, file: StaticString, line: UInt)
     func assert(collectionViewWith matcher: Matcher, hasSectionCount sectionCount: Int, file: StaticString, line: UInt)
@@ -11,6 +13,28 @@ public protocol SenchaCollectionViewAssertions: EarlGreyHumanizer {
 }
 
 public extension SenchaCollectionViewAssertions {
+
+    func assertCollectionViewIsEmpty(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        assert(collectionViewWith: matcher, hasCellCount: 0, file: file, line: line)
+    }
+
+    func assertCollectionViewIsNotEmpty(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        let cellCountAssert = GREYAssertionBlock(name: "CollectionView is not empty") { (element, error) -> Bool in
+            if let collectionView = element as? UICollectionView {
+                let numberOfCells = collectionView.numberOfItems(inSection: 0)
+                return numberOfCells > 0
+            }
+            return false
+        }
+
+        select(
+            matcher,
+            file: file,
+            line: line
+        ).assert(
+            cellCountAssert
+        )
+    }
 
     func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, file: StaticString = #file, line: UInt = #line) {
     
@@ -21,7 +45,6 @@ public extension SenchaCollectionViewAssertions {
             file: file,
             line: line
         )
-        
     }
 
     func assert(collectionViewWith matcher: Matcher, hasCellCount cellCount: Int, inSection section: Int, file: StaticString = #file, line: UInt = #line) {
@@ -38,8 +61,8 @@ public extension SenchaCollectionViewAssertions {
             matcher,
             file: file,
             line: line
-            ).assert(
-                cellCountAssert
+        ).assert(
+            cellCountAssert
         )
     }
 
@@ -57,8 +80,8 @@ public extension SenchaCollectionViewAssertions {
             matcher,
             file: file,
             line: line
-            ).assert(
-                sectionCountAssert
+        ).assert(
+            sectionCountAssert
         )
     }
 }
--- Sencha/Assertions/SenchaTableViewAssertions.swift
+++ Sencha/Assertions/SenchaTableViewAssertions.swift
@@ -3,7 +3,9 @@ import UIKit
 import EarlGrey
 
 public protocol SenchaTableViewAssertions: EarlGreyHumanizer {
-    
+
+    func assertTableViewIsEmpty(with matcher: Matcher, file: StaticString, line: UInt)
+    func assertTableViewIsNotEmpty(with matcher: Matcher, file: StaticString, line: UInt)
     func assert(tableViewWith matcher: Matcher, hasRowCount rowCount: Int, file: StaticString, line: UInt)
     func assert(tableViewWith matcher: Matcher, hasRowCount rowCount: Int, inSection section: Int, file: StaticString, line: UInt)
     func assert(tableViewWith matcher: Matcher, hasSectionCount sectionCount: Int, file: StaticString, line: UInt)
@@ -11,7 +13,29 @@ public protocol SenchaTableViewAssertions: EarlGreyHumanizer {
 }
 
 public extension SenchaTableViewAssertions {
-    
+
+    func assertTableViewIsEmpty(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        assert(tableViewWith: matcher, hasRowCount: 0, file: file, line: line)
+    }
+
+    func assertTableViewIsNotEmpty(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        let cellCountAssert = GREYAssertionBlock(name: "TableView is not empty") { (element, error) -> Bool in
+            if let tableView = element as? UITableView {
+                let numberOfCells = tableView.numberOfRows(inSection: 0)
+                return numberOfCells > 0
+            }
+            return false
+        }
+
+        select(
+            matcher,
+            file: file,
+            line: line
+        ).assert(
+            cellCountAssert
+        )
+    }
+
     func assert(tableViewWith matcher: Matcher, hasRowCount rowCount: Int, file: StaticString = #file, line: UInt = #line) {
         
         assert(
