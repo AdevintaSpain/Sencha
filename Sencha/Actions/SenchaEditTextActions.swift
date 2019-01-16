af1064804f47025ffe21771759d16088f9a1df0a
Iván Ruiz Monjo <ivanruizmonjo@gmail.com>
2019-01-16 09:03:05 +0100

    added clears text action

--- Sencha/Actions/SenchaEditTextActions.swift
+++ Sencha/Actions/SenchaEditTextActions.swift
@@ -4,6 +4,7 @@ import EarlGrey
 public protocol SenchaEditTextActions: EarlGreyHumanizer {
     
     func type(text: String, inElementWith matcher: Matcher, file: StaticString, line: UInt)
+    func clearsTextInElement(with matcher: Matcher, file: StaticString, line: UInt)
 }
 
 public extension SenchaEditTextActions {
@@ -16,6 +17,17 @@ public extension SenchaEditTextActions {
             line: line
         ).perform(
             grey_typeText(text)
+        )
+    }
+    
+    func clearsTextInElement(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
+        
+        select(
+            matcher,
+            file: file,
+            line: line
+        ).perform(
+            grey_clearText()
         )
     }
 }
