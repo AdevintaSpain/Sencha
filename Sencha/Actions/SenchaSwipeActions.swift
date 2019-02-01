ce80f80e0c5791072e7337503cd9ed1e8a9f1822
Iván Ruiz Monjo <ivanruizmonjo@gmail.com>
2019-02-01 10:19:22 +0100

    changed direction implementation to no depend on greys enum

--- Sencha/Actions/SenchaSwipeActions.swift
+++ Sencha/Actions/SenchaSwipeActions.swift
@@ -10,20 +10,28 @@ public extension SenchaSwipeActions {
 
     func swipe(_ matcher: Matcher, inDirection direction: SenchaDirection,file: StaticString = #file, line: UInt = #line) {
 
-        let direction = GREYDirection(rawValue: direction.rawValue) ?? .left
         select(
             matcher,
             file: file,
             line: line
         ).perform(
-            grey_swipeSlowInDirection(direction)
+            grey_swipeSlowInDirection(direction.toGREYDirection())
         )
     }
 }
 
-public enum SenchaDirection: Int {
-    case left = 1
+public enum SenchaDirection {
+    case left
     case right
     case up
     case down
+
+    func toGREYDirection() -> GREYDirection {
+        switch self {
+        case .left: return .left
+        case .right: return .right
+        case .down: return .down
+        case .up: return .up
+        }
+    }
 }
