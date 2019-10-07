85ebd4e56427994416d76947c5c8b10814618f66
Andrey Chernoprudov <dinloq@gmail.com>
2019-10-07 22:11:50 +0500

    remove extension

--- Sencha/Extensions/XCTestCase+Sencha.swift
+++ Sencha/Extensions/XCTestCase+Sencha.swift
@@ -23,10 +23,10 @@ extension XCTestCase: Sencha {
         }
         
         if modally {
-            window.set(rootViewController: UIViewController())
+            window.rootViewController = UIViewController()
             window.rootViewController?.present(viewControllerToOpen, animated: true, completion: nil)
         } else {
-            window.set(rootViewController: viewControllerToOpen)
+            window.rootViewController = viewControllerToOpen
         }
     }
     
