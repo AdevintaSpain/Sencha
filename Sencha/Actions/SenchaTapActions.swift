2271db3874ddd6bfc3b2aa5f8914537acb1cbdb1
Gil Serra <gilserrap@gmail.com>
2021-04-27 16:28:42 +0200

    Added switch actions to change a switch's value Tap actions do not work with UISwitch under KIF, neither findview

--- Sencha/Actions/SenchaTapActions.swift
+++ Sencha/Actions/SenchaTapActions.swift
@@ -7,15 +7,6 @@ public protocol SenchaTapActions: EarlGreyHumanizer {
 
 extension XCTestCase: SenchaTapActions {
     public func tap(_ matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
-        switch matcher {
-        case .text(let text):
-            tester().tapView(withAccessibilityLabel: text)
-        case .accessibilityLabel(let label):
-            tester().tapView(withAccessibilityLabel: label)
-        case .accessibilityID(let accessibilityID):
-            tester().tapView(withAccessibilityIdentifier: accessibilityID)
-        default:
-            unsupportedTest(file: file, line: line)
-        }
+        tester().tap(nil, in: findView(with: matcher))
     }
 }
