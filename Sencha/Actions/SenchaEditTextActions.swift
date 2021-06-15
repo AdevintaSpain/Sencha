26911c5dea0d26e52303507905768248e6125f83
Daferpi <daferpi@protonmail.ch>
2021-06-15 09:40:26 +0200

    refactor edit test sencha and improve the way to test

--- Sencha/Actions/SenchaEditTextActions.swift
+++ Sencha/Actions/SenchaEditTextActions.swift
@@ -6,6 +6,7 @@ public protocol SenchaEditTextActions: EarlGreyHumanizer {
     func type(text: String, inElementWith matcher: Matcher, file: StaticString, line: UInt)
     func insertText(text: String, inElementWith matcher: Matcher, file: StaticString, line: UInt)
     func clearTextInElement(with matcher: Matcher, file: StaticString, line: UInt)
+    func clearAndEnterTextInElement(text: String, with matcher: Matcher, file: StaticString, line: UInt)
 }
 
 extension XCTestCase: SenchaEditTextActions {
@@ -17,6 +18,8 @@ extension XCTestCase: SenchaEditTextActions {
             tester().enterText(text, intoViewWithAccessibilityLabel: accessibilityLabelText)
         case .accessibilityLabel(let label):
             tester().enterText(text, intoViewWithAccessibilityLabel: label)
+        case .accessibilityID(_):
+            tester().enterText(text, into: nil, in: findView(with: matcher), expectedResult: text)
         default:
             unsupportedTest(file: file, line: line)
         }
@@ -35,6 +38,17 @@ extension XCTestCase: SenchaEditTextActions {
 
     public func clearTextInElement(with matcher: Matcher, file: StaticString = #file, line: UInt = #line) {
         tester().clearText(from: nil, in: findView(with: matcher))
+    }
+
+    public func clearAndEnterTextInElement(text: String, with matcher: Matcher, file: StaticString, line: UInt) {
+        switch matcher {
+        case .text(let accessibilityLabelText):
+            tester().clearText(fromAndThenEnterText: text, intoViewWithAccessibilityLabel: accessibilityLabelText)
+        case .accessibilityLabel(let label):
+            tester().clearText(fromAndThenEnterText: text, intoViewWithAccessibilityLabel: label)
+        default:
+            unsupportedTest(file: file, line: line)
+        }
     }
 
 }
