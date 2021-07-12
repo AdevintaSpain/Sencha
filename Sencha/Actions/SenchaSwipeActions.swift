111d6dc9e31e25496cee51bf86f3342e64091d6b
Gil Serra <gilserrap@gmail.com>
2021-07-12 12:38:13 +0200

    Migrate swipe actions to use KIF internally

--- Sencha/Actions/SenchaSwipeActions.swift
+++ Sencha/Actions/SenchaSwipeActions.swift
@@ -1,36 +1,5 @@
-import Foundation
-import EarlGrey
-
-public protocol SenchaSwipeActions: EarlGreyHumanizer {
-
-    func swipe(_ matcher: Matcher, inDirection direction: SenchaDirection, file: StaticString, line: UInt)
-    func swipeFast(_ matcher: Matcher, inDirection direction: SenchaDirection, file: StaticString, line: UInt)
-}
-
-public extension SenchaSwipeActions {
-
-    func swipe(_ matcher: Matcher, inDirection direction: SenchaDirection,file: StaticString = #file, line: UInt = #line) {
-
-        select(
-            matcher,
-            file: file,
-            line: line
-        ).perform(
-            grey_swipeSlowInDirection(direction.toGREYDirection())
-        )
-    }
-
-    func swipeFast(_ matcher: Matcher, inDirection direction: SenchaDirection,file: StaticString = #file, line: UInt = #line) {
-
-        select(
-            matcher,
-            file: file,
-            line: line
-        ).perform(
-            grey_swipeFastInDirection(direction.toGREYDirection())
-        )
-    }
-}
+import XCTest
+import KIF
 
 public enum SenchaDirection {
     case left
@@ -38,12 +7,40 @@ public enum SenchaDirection {
     case up
     case down
 
-    func toGREYDirection() -> GREYDirection {
+    var kifDirection: KIFSwipeDirection {
         switch self {
-        case .left: return .left
-        case .right: return .right
-        case .down: return .down
-        case .up: return .up
+        case .left:
+            return .left
+        case .right:
+            return .right
+        case .up:
+            return .up
+        case .down:
+            return .down
         }
+    }
+}
+
+public protocol SenchaSwipeActions: EarlGreyHumanizer {
+    func swipe(_ matcher: Matcher, inDirection direction: SenchaDirection, file: StaticString, line: UInt)
+    func swipeFast(_ matcher: Matcher, inDirection direction: SenchaDirection, file: StaticString, line: UInt)
+}
+
+extension XCTestCase: SenchaSwipeActions {
+
+    public func swipe(_ matcher: Matcher, inDirection direction: SenchaDirection,file: StaticString = #file, line: UInt = #line) {
+        switch matcher {
+        case .text(let label), .accessibilityLabel(let label):
+            tester().swipeView(withAccessibilityLabel: label, in: direction.kifDirection)
+        case .accessibilityID(let accessibilityID):
+            tester().swipeView(withAccessibilityIdentifier: accessibilityID, in: direction.kifDirection)
+        default:
+            unsupportedTest(file: file, line: line)
+        }
+    }
+
+    public func swipeFast(_ matcher: Matcher, inDirection direction: SenchaDirection,file: StaticString = #file, line: UInt = #line) {
+
+
     }
 }
